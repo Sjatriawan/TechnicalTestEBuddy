ffc544388f8bb7077995af9401f973dbba87bdd8
Sjatriawan <aryadendi@gmail.com>
2024-12-27 13:36:13 +0800

    Firebase Setup. Firestore, Storage, and Auth

---
+++ TechnicalTestEbuddy/TechnicalTestEbuddyApp.swift
@@ -0,0 +1,28 @@
+//
+//  TechnicalTestEbuddyApp.swift
+//  TechnicalTestEbuddy
+//
+//  Created by M Yogi Satriawan on 27/12/24.
+//
+
+import SwiftUI
+import FirebaseCore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+    return true
+  }
+}
+
+@main
+struct TechnicalTestEbuddyApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
